20ce9ec99b9246e60cd3d6ab67fa4fc9cd575744
rohanthomas <rohanthomas@rohanthomas-P453UA>
2021-07-11 21:59:46 +0530

    Commits for internl purpose

--- src/main/java/in/rohan/excelUtils/App.java
+++ src/main/java/in/rohan/excelUtils/App.java
@@ -1,5 +1,9 @@
 package in.rohan.excelUtils;
 
+import java.io.File;
+
+import in.rohan.excelUtils.Utils.ExcelUtils;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,19 @@ public class App
 {
     public static void main( String[] args )
     {
+//    	for(int i=0;i<47;i++) {
+//        	Student student=new Student();
+//        	student.setId(i);
+//        	student.setName("Student"+i);
+//        	ExcelUtils.getStudents().add(student);
+//        }
         System.out.println( "Hello World!" );
+        
+        ExcelUtils.readStudentsFromExcelSheet();
+        ExcelUtils.writeStudentsToExcelSheet();
+        ExcelUtils.writeStudentsToReport("11-12-2021");
+        ExcelUtils.readStudentsFromReport("1-12-2021");
+        ExcelUtils.getStudents();
+        
     }
 }
---
+++ src/main/java/in/rohan/excelUtils/Student.java
@@ -0,0 +1,39 @@
+package in.rohan.excelUtils;
+
+public class Student implements Comparable<Student>{
+    public int getStarCount() {
+        return starCount;
+    }
+
+    public void setStarCount(int starCount) {
+        this.starCount = starCount;
+    }
+
+    private int starCount;
+
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    private int id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+	@Override
+	public int compareTo(Student o) {
+		
+		return Integer.valueOf(getId()).compareTo(Integer.valueOf(o.getId()));
+	}
+}
